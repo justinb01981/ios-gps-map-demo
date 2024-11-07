4154b9eef0ac2344fc5c61b532f0661fc328bc73
Justin Brady <justin@domain17.net>
2024-11-07 09:44:59 -0800

    constraint fixes

--- GPSDemoApp/InputViewWithLabel.swift
+++ GPSDemoApp/InputViewWithLabel.swift
@@ -77,7 +77,7 @@ class TextViewWithLabel: UIView, BaseControlWithLabel {
             label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
             label.topAnchor.constraint(equalTo: topAnchor),
             label.bottomAnchor.constraint(equalTo: bottomAnchor),
-            heightAnchor.constraint(equalToConstant: 64)
+            //heightAnchor.constraint(equalToConstant: 64)
         ])
     }
     
